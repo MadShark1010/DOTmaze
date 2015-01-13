5894038f0b9e4edd8fb272cbb438a0b8e36da257
Michael Paynter <mikepaynter1010@gmail.com>
2015-01-13 17:47:17 -0500

    Initial Commit

---
+++ DOTmaze/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  DOTmaze
+//
+
+//  Copyright (c) 2015 Michael Paynter. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ DOTmaze/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  DOTmaze
+//
+
+//  Copyright (c) 2015 Michael Paynter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
